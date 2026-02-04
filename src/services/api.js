e1ecc60f1654c485baefbd4007e40eaea64523d0
rittichok14349-ux <rittichok14349@gmail.com>
2026-02-04 14:53:58 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://backendapi2026.onrender.com/';
+const BASE_URL = 'wedstd.huaiyot.ac.th';
 
 const api = axios.create({
     baseURL: BASE_URL,
